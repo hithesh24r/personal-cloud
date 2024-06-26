f222a12840124afd25ad52288f69833ffd52ff81
Hithesh Reddy Yaduguri <75219792+hithesh24r@users.noreply.github.com>
2024-06-26 21:59:41 +0530

    Update userdata.sh

--- userdata.sh
+++ userdata.sh
@@ -4,3 +4,5 @@ cd /home/ubuntu/
 git clone https://github.com/hithesh24r/personal-cloud.git
 mv /home/ubuntu/personal-cloud/* /home/ubuntu/
 sudo rm -rf personal-cloud
+chmod +x /home/ubuntu/script.sh
+. script.sh
